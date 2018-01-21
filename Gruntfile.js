d4fe6e1fd0ece6f36bc5b35fc3b980dd149cfcc9
Tom Fitton <thomas.fitton1991@gmail.com>
2018-01-21 11:58:00 +0000

    Add html transpilation

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,28 +3,50 @@ module.exports = function(grunt) { // jshint ignore:line
   "use strict";
 
   var directories = {
-    "appViews": "app/view/",
+    "appViews": {
+      "root": "app/view/",
+      "layouts": "app/view/layouts/",
+      "partials": "app/view/partials/"
+    },
+    "build": "build/",
     "dist": "dist/"
   };
 
   grunt.initConfig({
-    "clean": [directories.dist],
+    "clean": {
+      "dev": [directories.build],
+      "dist": [directories.dist]
+    },
     "copy": {
       "dist": {
         "files": [
           {
             "expand": true,
             "flatten": true,
-            "src": [directories.appViews + "*"],
+            "src": [directories.build + "*"],
             "dest": directories.dist
           }
         ]
       }
     },
+    "htmlbuild": {
+      "dist": {
+        "src": directories.appViews.layouts + "index.html",
+        "dest": directories.build,
+        "options": {
+          "beautify": true,
+          "sections": {
+            "partials": {
+              "meta": directories.appViews.partials + "meta.html"
+            }
+          }
+        }
+      }
+    },
     "watch": {
       "dev": {
         "files": [
-          directories.appViews + "*"
+          directories.appViews.root + "**"
         ],
         "tasks": ["build"],
         "options": {
@@ -37,7 +59,8 @@ module.exports = function(grunt) { // jshint ignore:line
   grunt.loadNpmTasks("grunt-contrib-clean");
   grunt.loadNpmTasks("grunt-contrib-copy");
   grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-html-build");
 
-  grunt.registerTask("build", ["clean", "copy"]);
+  grunt.registerTask("build", ["clean:dist", "htmlbuild", "copy", "clean:dev"]);
 
 };
